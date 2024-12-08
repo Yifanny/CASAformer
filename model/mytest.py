88d1398b93c132ba6952f5214e9a64c76529411a
Yifanny <zyfjane@live.com>
2024-12-08 23:10:11 +0800

    Update mytest.py

--- model/mytest.py
+++ model/mytest.py
@@ -7,7 +7,6 @@ import torch.nn as nn
 import datetime
 import time
 import matplotlib.pyplot as plt
-# from torchinfo import summary
 import yaml
 import json
 import sys
@@ -26,7 +25,7 @@ from lib.utils import (
 )
 from lib.metrics import RMSE_MAE_MAPE, std_MAE, RMSE_MAE_MAPE_under_diff_states
 from lib.my_data_prepare import get_dataloaders_from_index_data, read_distance_matrix
-from model.mySTAEformer import mySTAEformer
+from model.CASAformer import CASAformer
 
 # ! X shape: (B, T, N, C)
 
@@ -267,7 +266,7 @@ if __name__ == "__main__":
     dataset = args.dataset
     dataset = dataset.upper()
     data_path = f"../data/{dataset}"
-    model_name = mySTAEformer.__name__
+    model_name = CASAformer.__name__
 
     with open(f"{model_name}.yaml", "r") as f:
         cfg = yaml.safe_load(f)
@@ -275,7 +274,7 @@ if __name__ == "__main__":
 
     # -------------------------------- load model -------------------------------- #
 
-    model = mySTAEformer(**cfg["model_args"])
+    model = CASAformer(**cfg["model_args"])
 
     # ------------------------------- make log file ------------------------------ #
 
@@ -353,7 +352,7 @@ if __name__ == "__main__":
     print_log(f"Loss: {criterion._get_name()}", log=log)
     print_log(log=log)
     
-    model.load_state_dict(torch.load("../saved_models/mySTAEformer-0x-0x1-sparse_att_weighted_loss-METRLA-2024-07-21-21-41-36.ptweighted", map_location=DEVICE))
+    model.load_state_dict(torch.load("../saved_models/CASAformer-0x-0x1-sparse_att_weighted_loss-METRLA-2024-07-21-21-41-36.ptweighted", map_location=DEVICE))
     model.to(DEVICE)
 
     test_model(model, testset_loader, log=log)
